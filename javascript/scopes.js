ed6e72557ce5d15acc2c59c4644a64544581beef
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-10-17 09:39:06 +0530

    javascript

--- javascript/scopes.js
+++ javascript/scopes.js
@@ -1,3 +1,10 @@
+/* 
+ Two types of  scope
+ Global scope
+ Block scope
+*/
+
+
 let a = 10
 const b = 20
 var c = 30 
@@ -6,6 +13,13 @@ console.log(a);
 console.log(b);
 console.log(c);
 
+// check scope according to let , const , var
 if(true){
-    
+    let x = 'let variable'
+    const y = 'const variable'
+    var z = 'var variable'
 }
+
+// console.log(x); // not access outside of the scope
+// console.log(y); // Same as let x variable
+console.log(z); // working variable outside scope
