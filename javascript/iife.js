28066b02a74d313b4774d7411f64f6f5108f176d
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-10-20 01:36:35 +0530

    javascript

---
+++ javascript/iife.js
@@ -0,0 +1,26 @@
+// Immediately Invoked Function Expressions
+
+function expres(){
+    console.log(`Normal Function:DB Connected`);
+
+}
+
+expres();
+
+
+// ==============================
+// to remove polluted by the Global scope 
+(function expres1() {
+    console.log('Normal IIFE: DB Connected');
+
+})();
+
+// ==============================
+
+(() =>{
+    console.log('Using Arrow Fcuntion in IIFE: DB Connectedd2')
+})()
+
+
+
+
