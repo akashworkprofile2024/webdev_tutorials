50a010b4b6b4c0f7931425934ab125630858d526
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-10-21 01:22:58 +0530

    javascript

---
+++ javascript/iterations.js
@@ -0,0 +1,24 @@
+// for loop  basic 
+
+ for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element,'\n');
+    
+}
+// console.log(element,'br');
+
+// ===================================
+
+for (let index = 0; index <= 10; index++) {
+    const element1 = index
+    if(element1 == 5 ){
+        console.log(element1)
+    }
+    
+}
+
+
+
+
+
+
