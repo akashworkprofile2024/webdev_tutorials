2dceaac32b4b9c93c70fe6700886e841f3490863
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-11-05 10:46:37 +0530

    javascript

--- javascript/filtermap_reducein.js
+++ javascript/filtermap_reducein.js
@@ -26,4 +26,53 @@ const filternum1 = mynum.filter((storenum)=>{
 console.log(filternum1)
 
 
-// 
+// Filtering using arrow function if else condition
+console.log('\nFiltering using arrow function if else condition')
+
+const newnum = []
+
+mynum.forEach((num)=>{
+    if(num > 4){
+        newnum.push(num)
+    }
+})
+
+console.log(newnum)
+
+
+// Filter basic
+console.log('\nBasic Filter Operation')
+const books = [
+    {title: 'Book One', genre: 'Fiction', publish: 1981},
+    {title: 'Book Two', genre: 'Thriller', publish: 2000},
+    {title: 'Book Three', genre: 'Mystery', publish: 1999},
+    {title: 'Book Four', genre: 'Mystery', publish: 2011},
+    {title: 'Book Five', genre: 'Detective', publish: 2024}
+
+]
+
+
+let userbooks = books.filter((bk) => {
+       return bk.genre === 'Mystery' 
+} )
+userbooks = books.filter((bk) => {
+    return bk.publish <= 2000 && bk.genre === 'Fiction'
+})
+console.log(userbooks)
+
+
+ 
+// add number with array elements using map
+console.log('\nadd number with array elements using map')
+const addnum = mynum.map((number) => {
+    return number + 10
+})
+console.log(addnum)
+
+
+// map using chaining
+console.log('\nMap Using Chaining')
+const chanfunction = mynum.map((mulnum)=> mulnum * 10).map((addnum1)=>addnum1 + 1)
+
+console.log('Original Number: ',mynum,'\n',chanfunction)
+
