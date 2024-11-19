2b0ed755f0d3dd8285e97d0ac2afff0dbc95a17f
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-11-19 07:28:16 +0530

    javascript

---
+++ excercise/javascript/basic/13.js
@@ -0,0 +1,17 @@
+// Write a JavaScript exercise to create a variable using a user-defined name.  
+
+// declare a variable name
+let var_name = "avrh45"
+console.log(`var_name = ${var_name}`);
+// declare a variable name n and assign
+
+let temp = 120;
+
+// assign n into var_name
+
+this[var_name] = temp
+
+
+
+console.log(`var_name = ${this[var_name]}`);
+
---
+++ excercise/javascript/basic/14.js
@@ -0,0 +1,11 @@
+// Write a JavaScript exercise to get the filename extension. 
+
+var filename = '14.js';
+
+console.log(`Extension Before: ${filename.split('.').pop()}`);
+
+var filename = "adc.php";
+
+console.log(`Extension After: ${filename.split('.').pop()}`);
+
+
