d32b21fb9553eba04810d2f0b0dcabe8334c0d5c
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-12-13 00:39:28 +0530

    javascript

--- excercise/javascript/basic/23.js
+++ excercise/javascript/basic/23.js
@@ -1,15 +1,21 @@
 // Write a JavaScript program to create a new string from a given string by changing the position of the first and last characters. The string length must be broader than or equal to 1.
 
-function fl(str){
-    // Check if the length of str is less than or equal to 1
-    if(str.length <=1){
-        // If true, return str as is
-        return str;
+
+// Define a function named first_last that takes a parameter str1
+
+function first_last(str1){
+ 
+    // check if the length of str1 is less than or equal to 1
+    if(str1.length <= 1)
+    {
+        return str1;
     }
 
-    // extract the substring from the second character to the second-to-last character
-    mid_char = str.substring(1,str.length -1)
+    // Extract the substring from the second character to the second_to_last character of str1
 
-    // Return the last character to str followed by mid char
-    return(str.charAt(str.lr))
+    mid_char = str1.substring(1, str1.length -1 );
+    
+    // Return the last character of  str1 followed by mid_char and then first character of str1 
+
+    return (str1.charAt())
 }
