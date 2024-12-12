28e9183ead8c11fc1f1e5e6c0ccbed4c43408212
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-12-13 00:51:09 +0530

    javascript

---
+++ javascript/js_data_types/data_types_trails.js
@@ -0,0 +1,16 @@
+/*  String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+The Object Datatype
+
+The object data type can contain both built-in objects, and user defined objects:
+
+Built-in object types can be:
+
+objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more. 
+*/ 
