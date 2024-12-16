668088cc352ce94123c1598a46d2bef44667c01d
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-12-17 01:34:36 +0530

    javascript

---
+++ javascript/js_objects/js_object_properties.js
@@ -0,0 +1,16 @@
+let person = {
+    fname: 'Arjun',
+    lname:'Foester',
+    age:30,
+    email:'akash51.50500@gmail.com'
+}
+
+// Accessing JavaScript Properties
+
+console.log('full-details:',person,'\n')
+
+let age = person.age;
+console.log('Age:',age)
+
+let fname = person['fname']
+console.log('Name:',fname)
