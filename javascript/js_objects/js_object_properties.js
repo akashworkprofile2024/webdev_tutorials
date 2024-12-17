26b20971b8b6613ee9456285fd5458829f3e9c4b
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-12-17 11:28:49 +0530

    javascript

--- javascript/js_objects/js_object_properties.js
+++ javascript/js_objects/js_object_properties.js
@@ -9,8 +9,15 @@ let person = {
 
 console.log('full-details:',person,'\n')
 
+// METHOD 1
 let age = person.age;
 console.log('Age:',age)
 
+// METHOD 2
 let fname = person['fname']
 console.log('Name:',fname)
+
+// METHOD 3 
+let storefname = person.fname
+let storelname = person.lname
+console.log('Name: '+storefname+" "+'Lname: '+storelname);
