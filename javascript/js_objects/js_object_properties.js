076d2a0b6dfbbe6bf13dfbf7438f8c5c68b665c4
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2024-12-17 11:47:53 +0530

    javascript

--- javascript/js_objects/js_object_properties.js
+++ javascript/js_objects/js_object_properties.js
@@ -20,4 +20,49 @@ console.log('Name:',fname)
 // METHOD 3 
 let storefname = person.fname
 let storelname = person.lname
-console.log('Name: '+storefname+" "+'Lname: '+storelname);
+console.log('Name: '+storefname+" "+'Lname: '+storelname,'\n');
+
+
+// ADDING NEW PROPERTIES
+person.nationality =  'Indian';
+
+console.log("Name:"+person.fname+' '+'Fname:'+person.lname,'Nationality:'+person.nationality,'\n')
+
+
+// DELETE PROPERTIES
+console.log('Before Delete');
+console.log(person)
+
+delete person.nationality;    //delete person['nationality'];        
+console.log('After Delete');
+console.log(person,'\n');
+
+
+// NESTED OBJECTS
+
+let carsholder= {
+    firstname : 'Rahul',
+    lastname : 'Gupta',
+    carsmodel_obj : {
+        car1: 'Dodge Chargers',
+        car2: 'lambargini',
+        car3: 'GT mustang',
+    } 
+}
+
+// METHOD 1
+
+let store1 = carsholder.carsmodel_obj.car2
+console.log('Fetch_car2:',store1);
+
+// METHOD 2
+let store2 = carsholder['carsmodel_obj']['car1'];
+console.log("Fetch_car1: ",store2)
+
+// METHOD 3
+let holder = 'carsmodel_obj';
+let carmodel = 'car3';
+let store3 = carsholder[holder][carmodel]
+console.log('Fetch_car3:',store3)
+
+
